69eb08ca0d334309c9094d2c47e0fefbc57cf58b
Kamil Donda <dondakamil@gmail.com>
2021-05-23 14:16:47 +0200

    Update background.jsx

--- assets/background.jsx
+++ assets/background.jsx
@@ -5,9 +5,8 @@ function SvgComponent(props) {
   return (
     <Svg
       xmlns="http://www.w3.org/2000/svg"
-      width={393}
-      height={850.5}
-      viewBox="0 0 393 850.5"
+      width="100%"
+      height="100%"
       {...props}
     >
       <Path
