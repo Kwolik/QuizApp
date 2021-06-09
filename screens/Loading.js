cde2619081e1d8d31fc6514c5af5c805434b101a
Kamil Donda <dondakamil@gmail.com>
2021-06-09 20:18:10 +0200

    Add downloading data

--- screens/Loading.js
+++ screens/Loading.js
@@ -39,6 +39,36 @@ export default function Loading({ navigation }) {
 
   const [message, setMessage] = React.useState("Download data in progress...");
 
+  const getQuestionRequest = async () => {
+    // =======================================
+    //   TODO: REPLACE WITH GLOBAL VARIABLE
+    const CATEGORY = 19;
+    const AMOUNT = 10;
+    const DIFFICULTY = "medium";
+    const TYPE = "multiple";
+    // =======================================
+    const query = `amount=${AMOUNT}&category=${CATEGORY}&difficulty=${DIFFICULTY}&type=${TYPE}`;
+    const url = `https://opentdb.com/api.php?` + query;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+
+    if (responseJson.response_code == 0) {
+      // go to quiz view
+      setMessage("OK!");
+    } else if (responseJson.response_code == 1) {
+      setMessage("Error! No Results!");
+    } else if (responseJson.response_code == 2) {
+      setMessage("Error! Invalid Parameter!");
+    }
+  };
+
+  React.useEffect(() => {
+    getQuestionRequest();
+  }, []);
+
   return (
     <ImageBackground source={bg} style={styles.back} resizeMode="stretch">
       <View style={styles.container}>
@@ -46,12 +76,10 @@ export default function Loading({ navigation }) {
           <Image source={Logo} style={styles.logo} />
         </View>
         <View style={styles.horizontal}>
-          <ActivityIndicator size={100} color="#999999"/>
+          <ActivityIndicator size={100} color="#999999" />
         </View>
         <View>
-            <Text style={styles.text}>
-                {message}
-            </Text>
+          <Text style={styles.text}>{message}</Text>
         </View>
       </View>
     </ImageBackground>
