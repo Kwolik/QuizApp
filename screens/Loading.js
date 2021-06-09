989f5d7c2ec59441c3c7cf416db1a26e40451e43
Kamil Donda <dondakamil@gmail.com>
2021-06-09 19:31:27 +0200

    Add Loading screen and Navigation

---
+++ screens/Loading.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+
+import bg from "../assets/bg.png";
+import Logo from "../assets/logo.png";
+import BottomMenu from "../components/BottomMenu";
+
+export default function Loading({ navigation }) {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+    },
+    back: {
+      flex: 1,
+    },
+    button: {
+      width: 250,
+      height: 48,
+      backgroundColor: "#FF5656",
+      borderRadius: 10,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 50,
+    },
+    text: {
+      color: "white",
+      fontSize: 24,
+      fontWeight: "bold",
+    },
+    logo: {
+      width: 290,
+      height: 175,
+      marginTop: 185,
+    },
+  });
+
+  return (
+    <ImageBackground source={bg} style={styles.back} resizeMode="stretch">
+      <View style={styles.container}>
+        <View>
+          <Image source={Logo} style={styles.logo} />
+        </View>
+        <View>{startButton}</View>
+      </View>
+    </ImageBackground>
+  );
+}
